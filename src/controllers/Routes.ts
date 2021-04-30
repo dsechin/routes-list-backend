7453785ee062f59c377614d4aa060a96a2efcf62
Dmitry Sechin <dmitriy.sechin@gmail.com>
2021-04-30 17:32:16 +0300

    (chore) add a random delay to each API call response (< 500 ms)

--- src/controllers/Routes.ts
+++ src/controllers/Routes.ts
@@ -3,6 +3,18 @@ import {Service} from 'typedi';
 import {Route} from '../types';
 import {RoutesStorage} from '../storage/routes-storage';
 
+const delayBy = (value: unknown, delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(value), Math.max(delay, 0));
+  });
+};
+
+const delayRandomly = (value: unknown, maxDelay: number = 500) => {
+  const delay = Math.floor(Math.random() * Math.max(0, maxDelay));
+
+  return delayBy(value, delay);
+};
+
 @JsonController()
 @Service()
 export class RoutesController {
@@ -12,12 +24,16 @@ export class RoutesController {
 
   @Get('/routes')
   getAll() {
-    return this.routesStorage.getAll();
+    return delayRandomly(
+      this.routesStorage.getAll(),
+    );
   }
 
   @Get('/routes/:uuid')
   getOne(@Param('uuid') uuid: string) {
-    return this.routesStorage.getByUuid(uuid);
+    return delayRandomly(
+      this.routesStorage.getByUuid(uuid),
+    );
   }
 
   @Get('/route/for-ip/:ip')
@@ -25,21 +41,29 @@ export class RoutesController {
     @QueryParam('most-specific') mostSpecific: boolean,
     @Param('ip') ip: string,
   ) {
-    return this.routesStorage.getRouteForIp(ip, mostSpecific);
+    return delayRandomly(
+      this.routesStorage.getRouteForIp(ip, mostSpecific),
+    );
   }
 
   @Post('/routes')
   create(@Body() route: Omit<Route, 'uuid'>) {
-    return this.routesStorage.addRoute(route);
+    return delayRandomly(
+      this.routesStorage.addRoute(route),
+    );
   }
 
   @Put('/routes/:uuid')
   update(@Param('uuid') uuid: string, @Body({required: true}) route: Partial<Omit<Route, 'uuid'>>) {
-    return this.routesStorage.updateRoute(uuid, route);
+    return delayRandomly(
+      this.routesStorage.updateRoute(uuid, route),
+    );
   }
 
   @Delete('/routes/:uuid')
   remove(@Param('uuid') uuid: string) {
-    return this.routesStorage.removeRoute(uuid);
+    return delayRandomly(
+      this.routesStorage.removeRoute(uuid),
+    );
   }
 }
